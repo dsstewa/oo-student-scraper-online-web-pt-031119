967375d15480e1434879467ff29ea2f0c9d79047
David Stewart <davidstewart@gmail.com>
2019-04-27 20:32:27 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,10 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    html = open(index_url)
     binding.pry
+    
+    
   end
 
   def self.scrape_profile_page(profile_url)
