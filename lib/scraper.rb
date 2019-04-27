dc7dbb6fee4e489bb2fdee5f545031573d693b4f
David Stewart <davidstewart@gmail.com>
2019-04-27 22:20:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,7 +13,7 @@ class Scraper
       student = {}
       student[:name] = value.css("h4.student-name").text
       student[:location] = value.css("p.student-location").text
-      #student[:profile_url] = value.css("")
+      student[:profile_url] = value.css("")
       binding.pry
       
       
