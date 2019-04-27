20705ebcd1ce0987b2aef36401729454bae94072
David Stewart <davidstewart@gmail.com>
2019-04-27 23:48:14 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -29,9 +29,8 @@ class Scraper
         containers.css("div.social-icon-container").each do |social|
           
           
-          # social.children.css("a").each  do |next|
-          # binding.pry
-          #end
+          social.children.css("a")
+          end
           
          
          binding.pry
