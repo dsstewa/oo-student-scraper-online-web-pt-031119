dd4301dfe4c6ff374d63a9fbc34da7607adfc6f8
David Stewart <davidstewart@gmail.com>
2019-04-28 00:01:20 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -29,6 +29,9 @@ class Scraper
     doc.css("div.vitals-container").each do |containers|
         containers.css("div.social-icon-container").each do |social|
            social.children.css("a").each do |site_links|
+            
+            
+            
             links << site_links.attr("href")
   
               end
