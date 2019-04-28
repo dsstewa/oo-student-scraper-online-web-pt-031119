7c667ad1bd1174a71df620063a5bbc9ac981c5b1
David Stewart <davidstewart@gmail.com>
2019-04-28 00:55:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,7 +31,15 @@ end
 
 
 
-
+  # @name = student_hash[:name]
+  #   @location = student_hash[:location]
+  #   @twitter = student_hash[:twitter] if student_hash[:twitter]
+  #   @linkedin = student_hash[:linkedin] if student_hash[:linkedin]
+  #   @github = student_hash[:github] if student_hash[:github]
+  #   @blog = student_hash[:blog] if student_hash[:blog]
+  #   @profile_quote = student_hash[:profile_quote] if student_hash[:profile_quote]
+  #   @bio = student_hash[:bio] if student_hash[:bio]
+  #   @profile_url = student_hash[:profile_url] if student_hash[:profile_url]
 
 
 
